4e26d3d21d75d80541071dc67b1214487168c6cc
linghong <linghong_chen@hotmail.com>
2017-11-21 16:26:47 -0500

    added views.post_edit .

--- blog/views.py
+++ blog/views.py
@@ -14,16 +14,29 @@ def post_detail(request, pk):
 	post =get_object_or_404(Post, pk=pk)
 	return render(request, 'blog/post_detail.html', {'post':post})
 
+def valid_form_submission(form, request):
+	post = form.save(commit=False)
+	post.author = request.user
+	post.published_date = timezone.now()
+	post.save()
+	return redirect('post_detail', pk=post.pk)
+
 def post_new(request):
 	if request.method == "POST":
 		form=PostForm(request.POST)
 		if form.is_valid():
-			post = form.save(commit=False)
-			post.author = request.user
-			post.published_date = timezone.now()
-			post.save()
-			return redirect('post_detail', pk=post.pk)
-
+			valid_form_submission(form, request)
 	else:
 		form = PostForm()
 	return render(request, 'blog/post_new.html', {'form':form})
+
+def post_edit(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+
+	if request.method =="POST":
+		form =PostForm(request.POST, instance = post)
+		if form.is_valid():
+			valid_form_submission(form, request)
+	else:
+		form =PostForm(instance=post)
+	return render(request, 'blog/post_new.html', {'form': form})
